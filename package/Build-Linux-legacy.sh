7cfe2c30be576a2d0de3ab5da7ff60589b3aef40
Toporin <satochip.wallet@gmail.com>
2024-07-04 09:35:48 +0100

    Github action build linux legacy correct again script

--- package/Build-Linux-legacy.sh
+++ package/Build-Linux-legacy.sh
@@ -3,16 +3,16 @@
 # Build Debian/Ubuntu binary package release
 
 
-if ! (cat /etc/os-release | grep -E "22\.04(\.[0-9]+)? LTS \(Jammy Jellyfish\)" > /dev/null ) then
-  echo "This building script only runs on Ubuntu 22.04."
-  exit 1
-fi
+#if ! (cat /etc/os-release | grep -E "22\.04(\.[0-9]+)? LTS \(Jammy Jellyfish\)" > /dev/null ) then
+#  echo "This building script only runs on Ubuntu 22.04."
+#  exit 1
+#fi
 
-#function is_installed {
-#  if [ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-#      echo 'not'
-#    fi
-#}
+function is_installed {
+  if [ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+      echo 'not'
+    fi
+}
 
 softs_req=('libnotify4' 'libgtk-3-0' 'libpcsclite-dev' 'libsdl2-dev' 'python3-venv' 'python3-pip')
 

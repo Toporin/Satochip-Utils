0c984597f5ad38e37f2a23dda1175c9a2bf1b0ab
Toporin <satochip.wallet@gmail.com>
2024-07-04 11:23:20 +0100

    Github action build macos more fix for dmg

--- package/Build-Mac.sh
+++ package/Build-Mac.sh
@@ -42,6 +42,6 @@ echo DMG building requires biplist and dmgbuild.
 
 python -m pip install biplist
 python -m pip install dmgbuild
-python -m dmgbuild -s packages/dmg_build.py
+python -m dmgbuild -s packages/dmg_build.py volume_name Satochip-Utils.dmg
 ls -l
 ls -l dist/
